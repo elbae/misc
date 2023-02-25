23fd9d287201bea1d671f69cff5f73b793a2096e
elbae <baessoandrea91@gmail.com>
2023-02-25 19:44:30 +0100

    Create synack.sh
    Not the file you are looking for

---
+++ synack.sh
@@ -0,0 +1,52 @@
+!/usr/bin/bash
+
+export ME=$(whoami | tr -d '\n')
+
+sudo apt update
+sudo apt install aptitude
+sudo chown -R $ME: /opt/ 
+sudo aptitude install zsh
+sudo aptitude install sublime-text
+
+git clone https://github.com/danielmiessler/SecLists.git /opt/SecLists
+cd ~/Downloads/
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall && rm msfinstall
+pwd
+wget -q https://github.com/projectdiscovery/httpx/releases/download/v1.2.7/httpx_1.2.7_linux_amd64.zip
+sleep 1
+
+unzip httpx_1.2.7_linux_amd64.zip
+rm ~/Downloads/httpx_1.2.7_linux_amd64.zip  &
+rm ~/Downloads/LICENSE.md &
+rm ~/Downloads/README.md &
+mv ~/Downloads/httpx /opt/httpx
+
+file /opt/httpx
+
+wget -q https://github.com/projectdiscovery/nuclei/releases/download/v2.8.9/nuclei_2.8.9_linux_amd64.zip
+sleep 1
+unzip nuclei_2.8.9_linux_amd64.zip
+rm ~/Downloads/nuclei_2.8.9_linux_amd64.zip &
+mv ~/Downloads/nuclei /opt/nuclei
+file /opt/nuclei
+
+cd
+
+
+sudo aptitude install -y libssl-dev libffi-dev python-dev-is-python3 build-essential
+curl -sSL https://install.python-poetry.org | python3 -
+curl -sSL https://install.python-poetry.org | python3 -
+export PATH="~/.local/bin:$PATH"
+git clone https://github.com/Porchetta-Industries/CrackMapExec /opt/CrackMapExec
+cd /opt/CrackMapExec
+sudo aptitude install python3-pip
+~/.local/bin/poetry lock --no-update
+~/.local/bin/poetry install
+~/.local/bin/poetry run crackmapexec
+
+alias crackmapexec='cd /opt/CrackMapExec; ~/.local/bin/poetry run crackmapexec'
+
+cd ~/Downloads/
+wget -q 'https://portswigger-cdn.net/burp/releases/download?product=pro&version=2023.1.2&type=Linux' -O burppro.sh
+chmod +x burppro.sh
+./burppro.sh
